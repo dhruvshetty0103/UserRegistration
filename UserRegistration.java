ef866253baaca037e127c3450dc34dc18a62a054
dhruvshetty0103 <dhruvshetty0103@gmail.com>
2021-09-13 11:14:33 +0530

    Refactored:Valid first name needs to be checked and return true is it's correct and comments added

--- UserRegistration.java
+++ UserRegistration.java
@@ -4,6 +4,9 @@ import java.util.regex.Pattern;
 
 public class UserRegistration 
 {
+	/* The below validateFirstName function will check for whether it satisfy
+	 * pattern if yes then it returns boolean value true for it else false
+	 */
 		public boolean validateFirstName(String firstName) 
 		{
 			String pattern = "^[A-Z][a-zA-Z]{2,}";
