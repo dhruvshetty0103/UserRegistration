09987641cc2c7000353d5cc06b2537e9b4cb7a44
dhruvshetty0103 <dhruvshetty0103.gmail.com>
2021-09-13 19:52:38 +0530

    Added:Junit testing to have at least one upper case character in the password

--- UserRegistrationPasswordTesting.java
+++ UserRegistrationPasswordTesting.java
@@ -10,7 +10,7 @@ import java.util.Collection;
 import org.junit.Before;
 
 @RunWith(Parameterized.class)
-public class PasswordMinLengthTesting 
+public class UserRegistrationPasswordTesting 
 {
 	boolean expectedResult;
 	String password;
@@ -21,7 +21,7 @@ public class PasswordMinLengthTesting
 		obj=new UserRegistration();
 	}
 	
-	public PasswordMinLengthTesting(String password,boolean expectedResult) 
+	public UserRegistrationPasswordTesting(String password,boolean expectedResult) 
 	{
 		this.password=password;
 		this.expectedResult=expectedResult;
@@ -33,13 +33,13 @@ public class PasswordMinLengthTesting
 	{
 		return Arrays.asList(
 				new Object[][] {
-						{"917574068586",true},//here passing some test cases for first names and expected results
-						{"91",false},
-						{"dfsdfjdgfdh",true},
+						{"917A574068586",true},//here passing some test cases for first names and expected results
+						{"91D",false},
+						{"dfsAdfjdgfdh",true},
 						{"75740",false},
-						{"aadsadas",true},
-						{"abcblsasco",true}
-						
+						{"aaAdsadas",true},
+						{"abcb#Alsasco",true},
+						{"ABCDEFG",false}						
 				});
 	}
 	
